c5d3fa97370c9d586c08894b5661774a6026a987
Seungin Lyu <seungin.lyu@students.olin.edu>
2018-02-07 23:15:59 -0500

    Add my first jest tests

--- database.js
+++ database.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/goober';
+
+connectToDB(mongoURI);
 
 var database = {}; // wrapper for databse module
 database.connecToDB = connectToDB;
+module.exports = database;
 
 function connectToDB(url) {
   mongoose.connect(url);
@@ -12,5 +16,3 @@ function connectToDB(url) {
     console.log("Connected to mongodb on", url);
   });
 }
-
-module.exports = database;
--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const port = process.env.PORT || 8080;
-const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/goober';
+
 const app = require('./app');
 const db =  require('./database');
-
-db.connecToDB(mongoURI);
 
 app.listen(port, function() {
   console.log("Goober. running on port", port);

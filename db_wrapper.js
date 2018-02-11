19fa0d0704ac253dcfd5c1e7f42efd141f4be9b1
Seungin Lyu <seungin.lyu@students.olin.edu>
2018-02-11 01:49:50 -0500

    Add create new goo feature

--- db_wrapper.js
+++ db_wrapper.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/goober';
+mongoose.Promise = global.Promise;
 
 connectToDB(mongoURI);
 
-var database = {}; // wrapper for databse module
-database.connecToDB = connectToDB;
-module.exports = database;
+const Goo = require('./models/Goo');
+var db_wrapper = {}; // wrapper for database module
+
+db_wrapper.connecToDB = connectToDB;
+db_wrapper.Goo = Goo;
+
+module.exports = db_wrapper;
 
 function connectToDB(url) {
   mongoose.connect(url);
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const port = process.env.PORT || 8080;
 
 const app = require('./app');
-const db =  require('./database');
 
 app.listen(port, function() {
   console.log("Goober. running on port", port);

a10cd35d2f9d6d9f1d4fa12578fdff7b80b9f079
Seungin Lyu <seungin.lyu@students.olin.edu>
2018-02-17 15:33:46 -0500

    Add react-router settings

---
+++ frontend/src/js/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import GooForm from './GooForm'
+
+const Home = () => (
+  <div>
+    <h1>Welcome to Goober.</h1>
+    <div>
+      <GooForm/>
+    </div>
+  </div>
+)
+
+export default Home

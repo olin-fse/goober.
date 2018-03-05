dad8774caf93e383efa77e3d951a8bd48f8c09d8
Seungin Lyu <seungin.lyu@students.olin.edu>
2018-03-05 17:27:25 -0500

    Seungin/e2e (#5)
    * Add e2e setup, add spy attempt
    * Add travis config

--- __tests__/db.test.js
+++ __tests__/db.test.js
@@ -19,14 +19,13 @@ describe('Test connectToDB function of db_wrapper', () => {
         });
     });
     // test('When given a wrong MongoURI, it should respond with error', (done) => {
-    //     function connectToWrongDB(){
-    //         const wrongURI = 'iamnotamongodab';
-    //         return db.connectToDB(wrongURI);
-    //     }
-    //     res = connectToWrongDB();
+    //     // lets use spy instead
+    //     const spy = jest.spyOn(db, 'connectToDB');
+    //     const isConnecting = db.connectToDB('wrongguri');
+    //     expect(spy).rejects.toThrow();
+    //         // expect(res.readyState).toBe(0);
+    //         done();
     //     //todo connectTODB is not throwing errors that throwError can catch
-    //     expect(res.readyState).toBe(2);
-    //     done();
     // });
 });
 
@@ -34,8 +33,10 @@ describe('Test Goo Model', () => {
     beforeAll(() => {
         return mongoose.connect(mongoTestURI);
     });
-    afterAll(() =>{
-        return mongoose.disconnect();
+    afterAll(() => {
+        return db.deleteAllGoo().then(() => {
+            mongoose.disconnect();
+        });
     });
     test('deleteAllGoo should throw error when NODE_ENV is not test', (done) => {
         function deleteAllGoo(){
@@ -44,6 +45,18 @@ describe('Test Goo Model', () => {
         }
         expect(deleteAllGoo).toThrowError("InvalidEnvironmentError");
         process.env.NODE_ENV = 'test';
+        done();
+    });
+    test('saveGoo() should throw error when values have wrong data types', (done) => {
+        const testGoo = {title:12312, location:12312, description:121212,
+                         tags:[], people:[], title:'', location:''};
+        function saveGoo(){
+            db.Goo.saveGoo(testGoo);
+        }
+        // expect(saveGoo).toThrowError("");
+        done();
+    });
+    test('deleteGoo() should throw error when there is no match', (done) => {
         done();
     });
 });

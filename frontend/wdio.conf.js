fae87b36cd3c8e8ad41d2996332116dfdc803bfa
Seungin Lyu <seungin.lyu@students.olin.edu>
2018-03-10 21:00:02 -0500

    fix error

--- frontend/wdio.conf.js
+++ frontend/wdio.conf.js
@@ -44,7 +44,7 @@ exports.config = {
         // 5 instances get started at a time.
         maxInstances: 5,
         //
-        browserName: 'chrome', 'ChromeHeadless',
+        browserName: 'chrome',
         chromeOptions:{
             args: ['--headless', '--disable-gpu'],
         },

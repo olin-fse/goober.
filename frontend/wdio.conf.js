bcbafe55e86d91a8d8be0ff692cb6c087a80821e
Seungin Lyu(승인) <seungin.lyu@students.olin.edu>
2018-04-18 18:01:13 -0400

    Seungin/docker setup (#10)
    * Add my first dockerfile
    * Add docker-compose
    * Fix hanging problem, trying to add chromedriver
    * Add e2e docker-copose
    * Update docker push only on master

--- frontend/wdio.conf.js
+++ frontend/wdio.conf.js
@@ -1,5 +1,6 @@
 exports.config = {
-
+    host: 'chrome',
+    port: 4444,
     //
     // ==================
     // Specify Test Files
@@ -45,9 +46,9 @@ exports.config = {
         maxInstances: 5,
         //
         browserName: 'chrome',
-        chromeOptions:{
-            args: ['--headless', '--disable-gpu'],
-        },
+        // chromeOptions:{
+        //    args: ['--headless', '--disable-gpu'],
+        //},
     }],
     //
     // ===================
@@ -81,6 +82,7 @@ exports.config = {
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
     baseUrl: 'http://localhost',
+
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
@@ -114,7 +116,8 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver', 'selenium-standalone'],
+    //services: ['chromedriver', 'selenium-standalone'],
+    services: ['chromedriver'],
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
     // see also: http://webdriver.io/guide/testrunner/frameworks.html
